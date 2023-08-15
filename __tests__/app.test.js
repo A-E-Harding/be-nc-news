660b380d1f05cc49deb95478dad5fa9d62829997
A-E-Harding <aeg.h@btinternet.com>
2023-08-15 14:30:19 +0100

    making required changes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const testData = require("../db/data/test-data/index");
 const seed = require("../db/seeds/seed");
 const connection = require("../db/connection");
 const endpoints = require("../endpoints.json");
+const articles = require("../db/data/test-data/articles.js");
 
 beforeEach(() => {
   return seed(testData);
@@ -76,32 +77,34 @@ describe("GET api/articles/:article_id", () => {
 });
 
 describe("GET /api/articles", () => {
-    test('200: responds with array of all article objects', () => {
-        return request(app).get("/api/articles")
-            .expect(200)
-            .then((response) => {
-                expect(response.body.length).toBe(13)
-                expect(Object.keys(response.body[0])).toEqual([
-                    "article_id",
-                    "title",
-                    "topic",
-                    "author",
-                    "created_at",
-                    "votes",
-                    "article_img_url",
-                    "comment_count"
-                ]);
-                
-        })
-    })
-})
-describe("ALL /notValidPath", () => {
-    test("404: responsds with custom error message when invalid path passed", () => {
-      return request(app)
-        .get("/api/articlezzz")
-        .expect(404)
-        .then(({ body }) => {
-          expect(body.msg).toBe("Path not found");
+  test("200: responds with array of all article objects", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.length).toBe(13);
+        expect(response.body).toBeSortedBy("created_at", { descending: true });
+        const article = response.body;
+        articles.forEach((article) => {
+          expect(article).hasOwnProperty("article_id", expect.any(Number));
+            expect(article).hasOwnProperty("title", expect.any(String));
+            expect(article).hasOwnProperty('topic', expect.any(String))
+            expect(article).hasOwnProperty('author', expect.any(String))
+            expect(article).hasOwnProperty('created_at', expect.any(String))
+            expect(article).hasOwnProperty('votes', expect.any(Number))
+            expect(article).hasOwnProperty('article_img_url', expect.any(String))
+            expect(article).hasOwnProperty('comment_count', expect.any(Number))
         });
-    });
+      });
   });
+});
+describe("ALL /notValidPath", () => {
+  test("404: responsds with custom error message when invalid path passed", () => {
+    return request(app)
+      .get("/api/articlezzz")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Path not found");
+      });
+  });
+});
