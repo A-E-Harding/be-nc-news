f84bfe26454ac048cb794033d4857f77a0eddf8b
A-E-Harding <aeg.h@btinternet.com>
2023-09-04 09:31:32 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const cors = require('cors');
 
 const { getTopics, getEndpoints, getArticles, getAllArticles, getArticleComments, postComment, removeComment, updateVotes, getAllUsers } = require("./controllers/controllers");
 
 
+
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
